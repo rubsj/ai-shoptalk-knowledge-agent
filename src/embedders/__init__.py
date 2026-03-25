995bf57e9e8fb4800e038403637ba7a99af33fb0
Ruby Jha <rubyjha@gmail.com>
2026-03-24 23:51:05 -0400

    feat(p5): add OpenAIEmbedder export and load_configs factory

--- src/embedders/__init__.py
+++ src/embedders/__init__.py
@@ -5,5 +5,6 @@ Models: MiniLMEmbedder (384d), MpnetEmbedder (768d), OpenAIEmbedder (1536d).
 
 from src.embedders.minilm import MiniLMEmbedder
 from src.embedders.mpnet import MpnetEmbedder
+from src.embedders.openai_embedder import OpenAIEmbedder
 
-__all__ = ["MiniLMEmbedder", "MpnetEmbedder"]
+__all__ = ["MiniLMEmbedder", "MpnetEmbedder", "OpenAIEmbedder"]
